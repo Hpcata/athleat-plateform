73ffc177f7c41c5ca9ec9ca0ba4e245da85ec4d0
Shekhar <219647733+shekhar086-cata@users.noreply.github.com>
2025-08-19 17:52:17 +0530

    MR4-338 - Removed unwanted class from close button
    MR4-338
    Removed unwanted class from close button in thank you modal in pre plan page

--- resources/views/front/pages/pre_plan_details.blade.php
+++ resources/views/front/pages/pre_plan_details.blade.php
@@ -1595,7 +1595,7 @@
                     <input type="hidden" name="redirect_url" id="thankYouModalRedirectUrl" value="">
                     <p class="mb-2">Your form is submitted.</p>
                     <p class="mb-4">Your plan will be created by Kez and sent via email in the coming days.</p>
-                    <button type="button" class="btn btn-primary w-50" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Close</button>
                 </div>
             </div>
         </div>
