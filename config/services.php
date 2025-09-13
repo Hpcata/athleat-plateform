bf66aec932d2877c9a8d251e43a0967766f33d23
Meet Kapadiya <meet@catalysta.com.au>
2025-09-13 22:48:55 +0530

    Remove default webhook secret from Stripe configuration to enhance security by requiring explicit environment variable definition.

--- config/services.php
+++ config/services.php
@@ -34,7 +34,7 @@ return [
         'key' => env('STRIPE_KEY'),
         'secret' => env('STRIPE_SECRET'),
         'public_key' => env('STRIPE_KEY'),
-        'webhook_secret' => env('STRIPE_WEBHOOK_SECRET', 'whsec_ES8CHO1zoBNxuomYXkvlHk2NNxOov7tv'),
+        'webhook_secret' => env('STRIPE_WEBHOOK_SECRET'),
         'testing_mode' => env('STRIPE_TESTING_MODE', false),
         'subscription_interval' => env('STRIPE_SUBSCRIPTION_INTERVAL', 'minute'), // month, day, minute
         'subscription_interval_count' => env('STRIPE_SUBSCRIPTION_INTERVAL_COUNT', 1),
