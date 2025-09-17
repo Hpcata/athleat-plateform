9f1b2151d48f8a8dd91fd421fc0bb0d3188bf957
ronak-catalysta <ronak@catalysta.com.au>
2025-09-17 13:02:00 +0530

    remove unnecessary scripts

--- resources/views/front/includes/style.blade.php
+++ resources/views/front/includes/style.blade.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" href="{{ frontAssets('css/bootstrap/bootstrap-5.3.min.css') }}" />
-<link rel="stylesheet" href="{{ frontAssets('css/style-1.css') }}" />
+{{-- <link rel="stylesheet" href="{{ frontAssets('css/style-1.css') }}" /> --}}
 <link rel="stylesheet" href="{{ frontAssets('css/styles.css') }}" />
 <link rel="stylesheet" href="{{ frontAssets('css/profile_landing.css') }}" />
 
@@ -12,7 +12,6 @@
 <link rel="stylesheet" href="{{ frontAssets('css/font-awesome/all.min.css') }}" media="print" onload="this.media='all'">
 
 <noscript>
-    <link rel="stylesheet" href="{{ frontAssets('css/font-awesome/all.min.css') }}">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap">
 </noscript>
 
